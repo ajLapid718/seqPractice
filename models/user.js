cb114f307e5182b1b8a9845ff8991597f1cd25e4
Allan James Lapid <ajLapid718@gmail.com>
2018-04-30 21:56:27 -0400

    Include relevant columns and ensure an email is not null

--- models/user.js
+++ models/user.js
@@ -2,7 +2,27 @@ const Sequelize = require('sequelize');
 const db = new Sequelize('postgres://localhost:5432/sequelize_practice', { logging: false });
 
 const User = db.define('user', {
-// YOUR CODE HERE...
+  first: {
+    type: Sequelize.STRING
+  },
+
+  last: {
+    type: Sequelize.STRING
+  },
+
+  age: {
+    type: Sequelize.INTEGER
+  },
+
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+
+  bio: {
+    type: Sequelize.TEXT
+  }
+
 }, {
 // ...AND HERE
 });
